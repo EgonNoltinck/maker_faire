0d345ec0acee19d0e8b5ed353929bb1892260933
Egon Noltinck <egon.noltinck@student.howest.be>
2021-01-22 15:27:35 +0100

    shop css

--- src/view/orders/shop.php
+++ src/view/orders/shop.php
@@ -1,1 +1,86 @@
-<h1>hallo3</h1>
+<section class="container">
+    <div class="header_shop section">
+        <div>
+        <h2 class="header_shop_title">Stel je bouwpakket samen</h2>
+        <p>We hebben een standaard pakket voor u klaar gezet. Heb je het materiaal al? Klik het gerust af want niemand wilt teveel materiaal hebben.</p>
+        </div>
+    </div>
+    <img class="fridge" src="./assets/img/smeg_blue.png" alt="fridge">
+    <h2 class=shop_title>Je benodigdheden</h2>
+    <div class="shop__box">
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="plank">
+                <h3 class="">€10</h3>
+                <p class="">Houten plank</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="houten plank">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>
+
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="vijzen">
+                <h3 class="">€10</h3>
+                <p class="">Vijzen</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="Vijzen">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>
+
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="Wielen">
+                <h3 class="">€10</h3>
+                <p class="">Wielen</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="Wielen">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>      
+
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="Motor">
+                <h3 class="">€10</h3>
+                <p class="">Motor</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="Motor">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>
+
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="Ontvanger">
+                <h3 class="">€10</h3>
+                <p class="">Ontvanger</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="Ontvanger">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>
+        
+        <div class="shop_items">
+            <article class="shop_item">
+                <img class="" src="" alt="verlengkabel">
+                <h3 class="">€10</h3>
+                <p class="">verlengkabel</p>
+                <form class="" action="" method="POST">
+                    <input type="hidden" name="" value="verlengkabel">
+                    <input class="button add__button" type="submit" value="Toevoegen" />
+                </form>
+            </article>
+        </div>
+
+    </div>
+
+
+</section>
