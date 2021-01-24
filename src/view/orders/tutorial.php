c3a13330f277b3f6526c7775a76ef733b7d34c12
Egon Noltinck <egon.noltinck@student.howest.be>
2021-01-24 20:06:10 +0100

    tutorial html css

---
+++ src/view/orders/tutorial.php
@@ -0,0 +1,73 @@
+<div class="containerTurt">
+    <section class=" container class class1">
+        <h2 class="hidden">Stap 1</h2>
+        <div class="tutorial_content">
+            <p class="tutorial_text ">Pak je platform en nagel 4 planken van 4cm hoog en 0,5cm breed aan de onderkant. vijs daarna in elke hoek een versterkingshoek toe. </p>
+            <img src="./assets/img/SVG/part1.svg" alt="part1">
+        </div>
+        <div class="tutorial_content tutorial_content--lower">
+            <p class="tutorial_text ">Boor daarna 4 gaten aan de zijkanten (2 aan elke kant) met een diameter van 12mm. Zorg ervoor dat de wielen de grond kunnen raken.</p>
+            <img src="./assets/img/SVG/part2.svg" alt="part2">
+        </div>
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP1</h1>
+        </div>
+    </section>
+
+    <section class="container class class2">
+        <h2 class="hidden">Stap 2</h2>
+        <div class="tutorial_content alone">
+            <p class="tutorial_text tutorial_text--alone">Pak de motoren en steek ze door de gaten van binnenuit af. Vijs deze vast (meet op voorhand af waar de gaten zitten) en lijm het vast voor extra stevigheid</p>
+            <img src="./assets/img/SVG/part3.svg" alt="part3">
+        </div>
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP2</h1>
+        </div>
+    </section>
+
+    <section class="container class class3">
+        <h2 class="hidden">Stap 3</h2>
+        <p class="tutorial_text ">Verbind nu de kabels tussen je motor en motor controller. Volg de instructies goed na op de afbeeldingen hieronder. Als alles vast zit mag je de controller vast lijmen met het lijmpistool </p>
+        <p class="tutorial_text ">Zorg ervoor dat je 1 rode en 1 zwarte gebruikt per motor</p>
+        <img src="./assets/img/SVG/part4.svg" alt="part4">
+        <img src="./assets/img/SVG/part5.svg" alt="part5">
+        <img src="./assets/img/SVG/part6.svg" alt="part6">
+        <img src="./assets/img/SVG/part7.svg" alt="part7">
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP3</h1>
+        </div>
+    </section>
+
+    <section class="container class class4">
+        <h2 class="hidden">Stap 4</h2>
+        <div class="tutorial_content tutorial_content--electronic">
+            <p class="tutorial_text">Nu gaan we de receiver verbinden. Gebruik hiervoor de jumper kabels. Lijm de receiver vast als deze klaar is. </p>
+            <img class="tutorial_img" src="./assets/img/SVG/part8.svg" alt="part8">
+        </div>
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP4</h1>
+        </div>
+    </section>
+
+    <section class="container class class5">
+        <h2 class="hidden">Stap 5</h2>
+        <div class="tutorial_content tutorial_content--electronic">
+            <p class="tutorial_text">Verbind de motor controller met het tussen stukje. Daarna kan je de batterij hierop vastklikken. Dit is voor de veiligheid. Plak de batterij vast als je hiermee klaar bent.</p>
+            <img class="tutorial_img" src="./assets/img/SVG/part10.svg" alt="part9">
+        </div>
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP5</h1>
+        </div>
+    </section>
+
+    
+    <section class="container class class7">
+        <h2 class="hidden">Stap 7</h2>
+        <div class="tutorial_content alone">
+            <p class="tutorial_text tutorial_text--alone">En klaar! Zet je frigo maar het platform en connecteer je controller met het platform! Veel plezier!</p>
+        </div>
+        <div class="flex about_info_color smol">
+            <h1 class="about_sideTitle about_sideTitle--tutorial ">STAP7</h1>
+        </div>
+    </section>
+</div>
